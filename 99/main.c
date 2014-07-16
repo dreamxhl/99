2f119f40e860882cb81a9f99bbd7fc7f77ed02de
mooter <mooter@mooter-no-MacBook-Air.local>
2014-07-16 15:56:46 +0900

    Initial Commit

---
+++ 99/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  99
+//
+//  Created by mooter on 2014/07/16.
+//  Copyright (c) 2014年 mooter. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
